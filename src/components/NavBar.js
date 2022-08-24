7b1069e777cafdc932861de6c1e5162b0141d82a
Maximiliano Benitez <maxxy_18@hotmail.com>
2022-08-23 21:55:08 -0300

    first commit

---
+++ src/components/NavBar.js
@@ -0,0 +1,34 @@
+const NavBar = () => {
+
+return (
+    <header>
+     <nav>   
+     <ul>
+    <li >
+      <a class="nav-link active" aria-current="page" href="#">PATITAS - PET SHOP</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="#">ALIMENTOS</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="#">ACCESORIOS</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href='#'>SERVICIOS</a>
+    </li>
+  <li class="nav-item">
+      <a class="nav-link" href="#">REGISTRATE</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="#">SING IN</a>
+    </li>
+    </ul>
+  </nav>
+  
+     </header>     
+ 
+   );
+ }
+ 
+ export default NavBar;
+ 
